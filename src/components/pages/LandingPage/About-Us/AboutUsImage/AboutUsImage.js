77be18223ead8b8e2f2bb1423167681fab7178e6
Anna Sanchez <annasanchez@Annas-MacBook-Pro.local>
2022-02-22 10:16:18 +0000

    aboutus

---
+++ src/components/pages/LandingPage/About-Us/AboutUsImage/AboutUsImage.js
@@ -0,0 +1,10 @@
+import styles from './style.module.css';
+
+function AboutImage(){
+
+    return (<>
+        <h1 className={styles.image}>About <span className={styles.image_Us}>Us</span></h1>
+    </>)
+}
+
+export default AboutImage;
