2c1a771b30cb941e8321af7db9893feddf0a8bf3
Charlotte <79761202+tinytecher@users.noreply.github.com>
2022-02-24 11:33:35 +0000

    added components to main page and started create experience form

--- src/components/pages/MainPage/index.js
+++ src/components/pages/MainPage/index.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import Form from "../../Form/Form";
+import Navbar from "../../Navbar/Navbar";
+import LandingPageSlider from "../LandingPage/landingPageSlider/landingPageSlider";
+import "./styles.css"
 
 function MainPage() {
-  return <h1>This is Main Page</h1>;
+  return (
+  <>
+    <Navbar />
+    <LandingPageSlider />
+    <Form />
+  </>
+  )
 }
 
 export default MainPage;
