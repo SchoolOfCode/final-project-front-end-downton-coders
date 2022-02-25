cdd312d43fae24be19e7ef6fdc0ce82a978444c8
Charlotte <79761202+tinytecher@users.noreply.github.com>
2022-02-25 10:59:11 +0000

    extra push

---
+++ plan.md
@@ -0,0 +1,11 @@
+## Tech stack
+
+## Front end delpoy on Netlify:
+https://xpeerience.netlify.app/
+
+## AFTER MVP is complete:
+- Login pop up on landing page
+- Chat rooms 
+  - general chat room on main page
+  - Specific chat room in each expereince
+- Give users the chance to give organisers feedback
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -9,11 +9,11 @@ function Hero() {
     <div className="heroWrapper">
 
     <header className = "nav-container">
-    <img  src ='logo2.png' width ='100px' height ="70px"  alt ="/#"/>
+    <img  src ='logo2.png' width ='100px' height ="70px"  alt ="experience logo"/>
       <nav className = "navlink">
         <ul>
-          <li><a href="/#">About</a></li>
-          <li><a href="/#">Login</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/login">Login</a></li>
         </ul>
       </nav>
     </header> 
