09d302a8e2d7e2a55b88e419849b158faba0beb9
Marydez <chukwumamary1@gmail.com>
2022-03-01 15:21:10 +0000

    removed register user function

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,23 +1,13 @@
 import React from 'react'
 import "../Navbar/navbar.css"
-import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa'
-import {useNavigate, Link} from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import { logout, reset } from '../features/auth/authSlice.js'
+import { FaSignInAlt,  } from 'react-icons/fa'
 
 //import { Link } from 'react-router-dom'
 
 
 function Navbar() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const { user } = useSelector((state) => state.auth)
 
-  const onLogout = () => {
-    dispatch(logout())
-    dispatch(reset())
-    navigate('/')
-  }
+  
   return (
     
     <nav className='nav-container'>
@@ -28,29 +18,9 @@ function Navbar() {
     <div className="navlink">
     <ul>
           <li><a href="/about">About</a></li>
+          <li><a href="/login"><FaSignInAlt />Login</a></li>
 
          
-        {user ? (
-          <li>
-            <button className='btn' onClick={onLogout}>
-              <FaSignOutAlt /> Logout
-            </button>
-          </li>
-        ) : (
-          <>
-            <li>
-              <Link to='/login'>
-                <FaSignInAlt /> Login
-              </Link>
-            </li>
-            <li>
-              <Link to='/register'>
-                <FaUser /> Register
-              </Link>
-            </li>
-          </>
-        )}
-
         </ul>
     </div>
     </nav>
--- src/components/pages/DashbroadPage/index.js
+++ src/components/pages/DashbroadPage/index.js
@@ -1,20 +1,6 @@
-import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import Spinner from '../../spinner/Spinner.js'
 
 function DashbroadPage() {
 
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-
-  const { user } = useSelector((state) => state.auth)
-
-  if (!user) {
-    navigate('/login')
-  }
-
-  
   return <h1>This is Dashbroad Page</h1>;
 
 }
